cdb919007bbb91c88b3004def1dcec0d3bc66bbc
Katherine Kelly <katherinewkwong@gmail.com>
2019-08-09 06:04:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,23 @@
-# Your Code Here
+def map(array)
+  new_arr = []
+  
+  array.each do |num|
+    new_arr << yield(num)
+  end
+  
+  new_arr
+end
+
+# map([1, 2, 3]) { |pizza| pizza * 2 } #think of this as an anonymous method (a regular method without a name, and yield just calls it **so dont need to give it a name)
+
+def reduce(array, starting_point = 0)
+  result = starting_point
+  i = 0
+  
+  while i < array.length
+    result ? yield(array[i], result)
+    i += 1
+  end
+  result
+end
+
