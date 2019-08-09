228fd859da051368a36908181d8a2affc025c61d
Katherine Kelly <katherinewkwong@gmail.com>
2019-08-09 06:16:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,7 +11,7 @@ end
 # map([1, 2, 3]) { |pizza| pizza * 2 } #think of this as an anonymous method (a regular method without a name, and yield just calls it **so dont need to give it a name)
 
 def reduce(array, starting_point = 0)
-  result = starting_point
+  result = false + 1
   i = 0
   
   while i < array.length
